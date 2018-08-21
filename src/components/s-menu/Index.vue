cf8824cfcbf67132386d11a67f967fd42650228f
邹荞蔓 <zouqm@dztec.net>
2018-08-21 17:36:42 +0800

    新增菜单

---
+++ src/components/s-menu/Index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+
+	</div>
+</template>
+<script>
+	
+	export default {
+		name: 's-menu',
+		props: {
+			visible: {
+				type: Boolean,
+				default: false
+			}
+		},
+		data: () => ({
+			sDialogVisible: false
+		}),
+	}
+</script>
+<style scoped>
+	
+</style>
